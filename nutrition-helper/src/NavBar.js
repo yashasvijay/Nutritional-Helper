621888fe45122606560f7436a4ae3becbbb5eb93
Rachel Kim <yseo7045@gmail.com>
2024-01-14 19:10:40 -0500

    Removed more unnecessary features, added the Login and NavBar .js and .css files, created Basic Navigation Bar and Login Screen Component

---
+++ nutrition-helper/src/NavBar.js
@@ -0,0 +1,36 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css';
+
+function BasicNavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" bg="myColor" variant="primary">
+      <Container>
+        <Navbar.Brand href="#home">Nutrition Helper</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Scanner</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Macro Tracker</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Allergen Tracker
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicNavBar;
