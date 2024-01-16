e039bcfffc2fb62efeba84c88901c3536bc5d26c
Mahathi Chinthapalli <149272104+MahathiChinthapalli@users.noreply.github.com>
2024-01-16 17:32:39 +0000

    user input database, connection to mongodb

--- nutrition-helper/src/App.js
+++ nutrition-helper/src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class App extends Component{
 
-function App() {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[]
+    }
+  }
+
+  componentDidMount(){
+    this.refreshItems();
+  }
+
+  async refreshItems(){
+    fetch("http://localhost:9000/getitems").then(response=>response.json())
+    .then(data=>{
+      this.setState({items:data});
+    })
+  }
+
+async addClick(){
+  var newItems=document.getElementById("newItems").value;
+  const data=new FormData();
+  data.append("newItems",newItems);
+
+  fetch("http://localhost:9000/additems",{
+    method:"POST",
+    body:data
+  }).then(res=>res.json())
+  .then((result)=>{
+    alert(result);
+    this.refreshItems();
+  })
+}
+
+async deleteClick(id){
+  fetch("http://localhost:9000/deleteitems?id="+id,{
+    method:"DELETE",
+  }).then(res=>res.json())
+  .then((result)=>{
+    alert(result);
+    this.refreshItems();
+  })
+}
+
+render() {
+  const{items}=this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h2>DATABASE</h2>
+        <input id="newItems"/>&nbsp;
+        <button onClick={()=>this.addClick()}>Add</button>
+      {items.map(item=>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Name: {item.name}... Calories: {item.calories}</b>&nbsp;
+          <button onClick={()=>this.deleteClick(item.id)}>Delete</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
+}
 }
 
 export default App;
